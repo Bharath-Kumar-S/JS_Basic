3d53d6a6048b5cf0323349fb43da3d856180af90
Bharath Kumar <bharathkumarkarthick@gmail.com>
2019-03-14 18:13:26 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,31 @@
+let a = [18, 25, 34, 43, 56, 60];
+
+// for (let i = 0; i < a.length; i++) {
+//     console.log(a[i]);
+// }
+
+// a.forEach((e,i) => {
+//     console.log(`${e} is of index ${i}`);
+// })
+
+console.log(Array.isArray(a));
+
+a.shift()
+
+console.log(a.includes(18));
+
+// console.log(a.;
+
+let t = `Anything is fine`;
+
+let b = t.split(` `);
+
+console.log(b);
+
+console.log(typeof(t))
+
+console.log(typeof(b))
+
+console.log(a.indexOf(34));
+
+a.shift(2);
---
+++ array_obj.js
@@ -0,0 +1,9 @@
+// let a = [{name : `Bharath`, dep : `IT`},{name : `Ashwin`, dep : `IT`},{name : `Arun`, dep : `IT`},{name : `Karthick`, dep : `IT`}]
+
+// console.log(a[0].dep);
+
+const d = {
+    name: `Shivani`, dept: `Testing`, role: `tester`, phnno: 12345678, automation_tester: false
+}
+
+console.log(d.name);
